99ea8a719293670f7b30bbf0dd918f812594b85b
Marie Agosta <M.Agosta@lse.ac.uk>
2016-09-30 15:15:03 +0200

    Delete R file for Assignment 1.R

--- R file for Assignment 1.R
+++
@@ -1,9 +0,0 @@
-#Partner Project Beginning 
-#setwd('Github/Partner_for_real')
-data()
-
-?state.abb (state)
-
-#Testing whether making changes works. 
-#Quick suggestion: I think the file name is a bit long and has
-#too many spaces. 
